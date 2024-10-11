ddda9524393e17ec9da2e4a83990211fc3175914
koufouda <koufoudakisy@gmail.com>
2024-10-11 10:31:38 +0300

    admin modifications

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -36,4 +36,14 @@ class ModelTests(TestCase):
     def test_new_user_without_email_raises_error(self):
         """Test that creating user without an email raises a ValueError"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('','test123')
+            get_user_model().objects.create_user('  ','test123')
+
+    def test_create_superuser(self):
+        """Test Creating superuser."""
+        user = get_user_model().objects.create_superuser(
+                'test@superuser.gr',
+                'superuser',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
