343c220ef95ce922da17c12b2f3db49c26f9e596
koufouda <koufoudakisy@gmail.com>
2024-10-11 10:33:14 +0300

    admin modifications

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -5,6 +5,7 @@ Tests for the Django admin modifications
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.test import Client
 
 
