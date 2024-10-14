55d018d7acd9fbf1f2818ca14b18b768c4736b23
koufouda <koufoudakisy@gmail.com>
2024-10-14 10:43:59 +0300

    add User Serializers 3

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,19 +1,20 @@
 """
-Serializes for the user API
+Serializers for the user API View.
 """
 
 from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
-class UserSerializer (serializers.ModelSerializer):
+
+class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user model"""
 
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_Kwargs = {'password': {'write_only': True, 'min_length': 5}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
-    def create (self, validated_data):
+    def create(self, validated_data):
         """Create user and return it"""
         return get_user_model().objects.create_user(**validated_data)
